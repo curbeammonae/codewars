f46d9957229cdbc817648cab097f8bb7a9cb45bf
Monae Curbeam <curbeammonae@gmail.com>
2022-11-22 18:39:38 -0500

    codewars

--- main.js
+++ main.js
@@ -19,3 +19,12 @@ var championsLeagueGoals = 10
 var copaDelReyGoals = 5
 
 var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+
+//Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+   i = i * 2
+    return i;
+  }
